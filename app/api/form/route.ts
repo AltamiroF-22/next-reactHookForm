3e5217841b73b3a22ee06f6ba03bf54170e99ed7
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-07-16 21:57:40 -0300

    image , api\prisma

---
+++ app/api/form/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { name, lastname, address, block, number, imageSrc, phoneNumber } =
+    body;
+
+  const form = await prisma.form.create({
+    data: {
+      name,
+      lastname,
+      address,
+      block,
+      number,
+      imageSrc,
+      phoneNumber,
+    },
+  });
+
+  return NextResponse.json(form);
+}
--- app/components/form.tsx
+++ app/components/form.tsx
@@ -2,11 +2,19 @@
 
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "./inputs/input";
+import ImageUpload from "./inputs/imageInput";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const Form = () => {
+  const router = useRouter();
+
   const {
     register,
     handleSubmit,
+    setValue,
+    watch,
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
@@ -14,67 +22,108 @@ const Form = () => {
       lastname: "",
       address: "",
       number: "",
-      block:""
+      block: "",
+      phoneNumber: "",
+      imageSrc: "",
     },
   });
 
+  const imageSrc = watch("imageSrc");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log("credentials", data);
+    if (!data) {
+      toast.loading("Somehing went wrong!");
+      return;
+    }
+
+    axios
+      .post("/api/form", data)
+      .then(() => {
+        toast.success("Form sent with success!");
+        router.refresh();
+      })
+      .catch(() => {
+        toast.error("Something went wrong!");
+      });
   };
 
   return (
     <div className="flex flex-col gap-3">
       <Input
         id="name"
-        placeholder="name:"
+        placeholder="Name*"
         register={register("name", {
-          required: "Name is required",
+          required: "Name is required!",
           minLength: {
             value: 9,
-            message: "Name must be at least 9 characters long",
+            message: "Name must be at least 9 characters long!",
           },
         })}
         errors={errors}
       />
       <Input
         id="lastname"
-        placeholder="lastname:"
+        placeholder="Lastname*"
         register={register("lastname", {
-          required: "Lastname is required",
+          required: "Lastname is required!",
         })}
         errors={errors}
       />
-     <div className="grid grid-cols-4 gap-4">
-  <div className="col-span-2">
-    <Input
-      id="address"
-      errors={errors}
-      register={register("address", { required: "address is required!" })}
-      placeholder="address:"
-    />
-  </div>
-  <div className="col-span-1">
-    <Input
-      id="block"
-      register={register("block", {
-        required: "the block?",
-      })}
-      errors={errors}
-      placeholder="block:"
-    />
-  </div>
-  <div className="col-span-1">
-    <Input
-      id="number"
-      register={register("number", {
-        required: "your house number?",
-      })}
-      errors={errors}
-      type="number"
-      placeholder="N:"
-    />
-  </div>
-</div>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-2">
+          <Input
+            id="address"
+            errors={errors}
+            register={register("address", { required: "Address is required!" })}
+            placeholder="Address*"
+          />
+        </div>
+        <div className="col-span-1">
+          <Input
+            id="block"
+            register={register("block", {
+              required: "The block?",
+            })}
+            errors={errors}
+            placeholder="Block*"
+          />
+        </div>
+        <div className="col-span-1">
+          <Input
+            id="number"
+            register={register("number", {
+              required: "Your house number?",
+            })}
+            errors={errors}
+            type="number"
+            placeholder="N*"
+          />
+        </div>
+      </div>
+
+      <Input
+        id="phoneNumber"
+        register={register("phoneNumber", {
+          required: "Phone number is required!",
+        })}
+        errors={errors}
+        placeholder="Phone number*"
+      />
+      <div className="w-full">
+        <ImageUpload
+          onChange={(value) => setCustomValue("imageSrc", value)}
+          value={imageSrc}
+        />
+      </div>
 
       <button
         onClick={handleSubmit(onSubmit)}
